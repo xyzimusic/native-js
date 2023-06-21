c75b6cb086695702f72364957db9b0c10c8b183d
XYZI <113294539+xyzimusic@users.noreply.github.com>
2023-06-21 22:31:49 +0300

    first commit

---
+++ src/01-hello-tests/01.test.ts
@@ -0,0 +1,57 @@
+import {multiply, splitIntoWords, sum} from "./01";
+
+test('sum should be correct', ()=>{
+    //data
+    const a = 1;
+    const b = 2;
+    const c = 3;
+
+    //action
+    const result1 = sum(a,b)
+    const result2 = sum(a,c)
+
+    //expect result
+    expect(result1).toBe(3);
+    expect(result2).toBe(4);
+})
+
+
+test('sum should be multiply', ()=>{
+    //data
+    const a =1;
+    const b =2;
+    const c =3;
+
+    //action
+    const result1 = multiply(a,b)
+    const result2 = multiply(a,c)
+
+    //expect result
+    expect(result1).toBe(2);
+    expect(result2).toBe(3);
+})
+
+test('splitting into words should be correct', ()=>{
+    //data
+    const sent1 = 'hello my friend'
+    const sent2 = 'JS - the best  programming language.'
+
+    //action
+    const result1 = splitIntoWords(sent1)
+    const result2 = splitIntoWords(sent2)
+    console.log(result2)
+
+    //expect result
+
+    expect(result1.length).toBe(3)
+    expect(result1[0]).toBe('hello')
+    expect(result1[1]).toBe('my')
+    expect(result1[2]).toBe('friend')
+    expect(result2.length).toBe(5)
+    expect(result2[0]).toBe('js')
+    expect(result2[1]).toBe('the')
+    expect(result2[2]).toBe('best')
+    expect(result2[3]).toBe('programming')
+    expect(result2[4]).toBe('language')
+
+})
---
+++ src/01-hello-tests/01.ts
@@ -0,0 +1,16 @@
+export function sum(a:number, b:number){
+    return a + b;
+}
+
+export function multiply(a:number, b:number){
+    return a * b;
+}
+
+export function splitIntoWords(sentense: string){
+    const words = sentense.toLowerCase().split(' ')
+    return words. filter(w=>w!=='' && w!== "-")
+        .map(w=>w
+        .replace("!","")
+        .replace(".","")
+        .replace(",",""))
+}
